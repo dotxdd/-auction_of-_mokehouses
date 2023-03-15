68608011f4c46bf136c2a05d981ea6db9ad49033
Gabriel Moskwa <gabriel.moskwa@wp.pl>
2023-03-15 20:49:27 +0100

    backend side of login

---
+++ resources/views/auth/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('title','Page title' )
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @parent
+
+@endsection
+
+@section('content')
+
+<div>Hello</div>
+@endsection
