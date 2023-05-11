6bf4539c37dc6783895ac9cbb51c4e099ab06dd3
Gabriel Moskwa <gabriel.moskwa@wp.pl>
2023-05-11 22:16:40 +0200

    tests and alerts

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 @section('title','Page title' )
 @section('title', 'Page Title')
-
+@section('head_script')
+    <script>
+        function hideScrollbar() {
+            document.body.classList.add('hide-scrollbar');
+        }
+    </script>
+@endsection
 @section('sidebar')
     @parent
 
@@ -9,5 +15,11 @@
 
 @section('content')
 
-<div>Hello</div>
+    <div id="background-video-container" class="w-full w-full">
+        <video autoplay loop muted playsinline id="background-video">
+            <source src="https://videos.pond5.com/loading-wood-rustic-wood-oven-footage-110858202_main_xxl.mp4" type="video/mp4">
+        </video>
+
+        <div id="scroll-up-message">Cosy, isn't it? Welcome Back</div>
+    </div>
 @endsection
